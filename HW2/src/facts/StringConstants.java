a136f6662696f322aecb8cdd9446bfda1f105032
AlexLundin <aml140830@utdallas.edu>
2018-06-06 18:26:33 -0500

    Documentation Started

--- HW2/src/facts/StringConstants.java
+++ HW2/src/facts/StringConstants.java
@@ -7,7 +7,17 @@ package facts;
 // Edit this file to allow project to build correctly with your workspace path
 
 public interface StringConstants {
-	public static final String jsFile = "C:\\Users\\kim\\Desktop\\Screen-Cleaner\\Programs\\JavaEEWorkspace\\Test\\src\\facts\\facts.js";
-	public static final String xmlFile = "C:\\Users\\kim\\Desktop\\Screen-Cleaner\\Programs\\JavaEEWorkspace\\Test\\WebContent\\WEB-INF\\data\\facts.xml";
+	
+	// Java windows paths for Alex's machine #1
+	public static final String jsFile = "C:\\Users\\Alex\\Desktop\\Screen-Cleaner\\Programs\\JavaEEWorkspace\\Test\\src\\facts\\facts.js";
+	public static final String xmlFile = "C:\\Users\\Alex\\Desktop\\Screen-Cleaner\\Programs\\JavaEEWorkspace\\Test\\WebContent\\WEB-INF\\data\\facts.xml";
+	
+	
+	// Java windows paths for Alex's machine #2
+	//public static final String jsFile = "C:\\Users\\kim\\Desktop\\Screen-Cleaner\\Programs\\JavaEEWorkspace\\Test\\src\\facts\\facts.js";
+	//public static final String xmlFile = "C:\\Users\\kim\\Desktop\\Screen-Cleaner\\Programs\\JavaEEWorkspace\\Test\\WebContent\\WEB-INF\\data\\facts.xml";
+	
+	
+	// Java windows path for Webpage
 	public static final String thisServlet = "http://localhost:8080/Test/";
 }
